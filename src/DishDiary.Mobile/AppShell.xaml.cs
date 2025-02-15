cac8ba760c2cd757c5f0434cad1b9e92105a6fdd
Nerijus Sirvys <n.sirvys@senso.cloud>
2025-02-15 21:05:23 +0000

    initial MAUI project added

---
+++ src/DishDiary.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DishDiary.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/DishDiary.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DishDiary.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
